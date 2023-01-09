e6a1984ba3f105527673e2269de8a609fb709470
Roman Kamelianov <r.kamelyanov@yandex.ru>
2023-01-09 23:55:18 +0300

    feat: font normalize.css and headers desktop style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import logo from '../../images/logo.svg'
+import logo from '../../images/logo.svg';
+import socialTg from '../../images/social_telegram.svg';
+import socialInst from '../../images/social_insta.svg';
+import socialVk from '../../images/social_vk.svg';
+
+import './Header.css'
 
 function Header() {
   return (
     <header className="header">
       <a href='#'><img className="header__logo" src={logo} alt="Логотип"></img></a>
-      <div className="header__contacts">
-        <span className="header__contacts">Задайте интересующий вопрос</span>
-        <span className="contacts contacts_number">+7 777 777 77 77</span>
-        <a className="contacts contacts_tg" target="_blank" href="https://link_to_tg">@link_to_tg</a>
-        <a className="contacts contacts_inst" target="_blank" href="https://link_to_inst">@inst</a>
+      <div className="contacts__container">
+        <div className="header__contacts">
+          <span className="contacts__number">+7 777 777 77 77</span>
+          <a className="contacts__link" target="_blank" href="https://link_to_tg">
+            <img className="contacts__icon" src={socialTg} alt="Телеграм"></img>
+          </a>
+          <a className="contacts__link" target="_blank" href="https://link_to_inst">
+            <img className="contacts__icon" src={socialInst} alt="Инстаграм"></img>
+          </a>
+          <a className="contacts__link" target="_blank" href="https://link_to_vk">
+            <img className="contacts__icon" src={socialVk} alt="ВКонтакте"></img>
+          </a>
+        </div>
+        <span className="header__span">Задайте интересующий вопрос</span>
       </div>
     </header>
   );
