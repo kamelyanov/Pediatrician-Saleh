783552ca18043032121f7924d3f5923474e8b6d2
Roman Kamelianov <r.kamelyanov@yandex.ru>
2023-01-10 23:31:41 +0300

    feat: navbar layot, style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 
 import Header from "../Header/Header";
+import NavBar from '../NavBar/NavBar';
 
 function App() {
   return (
     <section className="page">
       <Header/>
+      <NavBar/>
     </section>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css'
+
+function NavBar () {
+  return ( 
+    <section className="navbar">
+      <ul className="navbar__links">
+        <li className="navbar__link">Обо мне</li>
+        <li className="navbar__link">Забота о здоровье</li>
+        <li className="navbar__link">Отзывы</li>  
+        <li className="navbar__link">Контакты</li>
+      </ul>
+    </section>
+  );
+
+};
+
+export default NavBar;
