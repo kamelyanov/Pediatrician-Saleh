ebd02482ada1821904b600fb8084789726ef4998
Roman Kamelianov <r.kamelyanov@yandex.ru>
2023-01-24 22:57:48 +0300

    feat: cards layout

--- src/components/SpecialList/SpecialList.js
+++ src/components/SpecialList/SpecialList.js
@@ -1,16 +1,16 @@
-import './SpecialList.css'
+import './SpecialList.css';
+import me from '../../images/me.jpg'
 
 function SpecialList ( ) {
 
   return (
     <section className="specialSection">
-      <button> left </button>
         <ul className="special_cards">
 
-          <li className="special_card">
+        <li className="special_card">
             <div className="card__container"> 
               <h3 className="card__title">Заголовок карточки</h3> 
-              <div className="card__image" ></div> 
+              <img src={me} className="card__image" alt="Моя фотография"></img> 
               <p className="card__description">Текст карточки тут что-то интересное типа там в тг на несколько строк</p>
               <button>Читать дальше =></button> 
               {/* тут открыватьеся попап с полным текстом и там же где-то должна быть кнопка поделиться в соцсети */}
@@ -20,7 +20,7 @@ function SpecialList ( ) {
           <li className="special_card">
             <div className="card__container"> 
               <h3 className="card__title">Заголовок карточки</h3> 
-              <div className="card__image" ></div> 
+              <img src={me} className="card__image" alt="Моя фотография"></img> 
               <p className="card__description">Текст карточки тут что-то интересное типа там в тг на несколько строк</p>
               <button>Читать дальше =></button> 
               {/* тут открыватьеся попап с полным текстом и там же где-то должна быть кнопка поделиться в соцсети */}
@@ -30,27 +30,17 @@ function SpecialList ( ) {
           <li className="special_card">
             <div className="card__container"> 
               <h3 className="card__title">Заголовок карточки</h3> 
-              <div className="card__image" ></div> 
+              <img src={me} className="card__image" alt="Моя фотография"></img> 
               <p className="card__description">Текст карточки тут что-то интересное типа там в тг на несколько строк</p>
               <button>Читать дальше =></button> 
               {/* тут открыватьеся попап с полным текстом и там же где-то должна быть кнопка поделиться в соцсети */}
             </div>
           </li>
 
-          <li className="special_card">
+                    <li className="special_card">
             <div className="card__container"> 
               <h3 className="card__title">Заголовок карточки</h3> 
-              <div className="card__image" ></div> 
-              <p className="card__description">Текст карточки тут что-то интересное типа там в тг на несколько строк</p>
-              <button>Читать дальше =></button> 
-              {/* тут открыватьеся попап с полным текстом и там же где-то должна быть кнопка поделиться в соцсети */}
-            </div>
-          </li>
-
-          <li className="special_card">
-            <div className="card__container"> 
-              <h3 className="card__title">Заголовок карточки</h3> 
-              <div className="card__image" ></div> 
+              <img src={me} className="card__image" alt="Моя фотография"></img> 
               <p className="card__description">Текст карточки тут что-то интересное типа там в тг на несколько строк</p>
               <button>Читать дальше =></button> 
               {/* тут открыватьеся попап с полным текстом и там же где-то должна быть кнопка поделиться в соцсети */}
@@ -58,7 +48,6 @@ function SpecialList ( ) {
           </li>
 
         </ul>
-      <button> left </button>
     </section>
   );
 };
